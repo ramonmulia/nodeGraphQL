dc0a22e8a669b7548daadc6f62c8ff8ad3a9b28b
Ramon <ramonmulia@gmail.com>
2018-03-21 23:09:53 -0300

    adding schema

---
+++ src/schema.js
@@ -0,0 +1,19 @@
+const {
+    GraphQLSchema,
+    GraphQLObjectType
+} = require('graphql');
+
+const queries = require('./queries');
+const mutations = require('./mutations');
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        fields: queries
+    }),
+
+    mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: mutations
+    })
+});
